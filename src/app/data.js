a7505b7d367979253953cab53c37c37cd066f96d
Francisco Sandoval <128339597+Franksanba@users.noreply.github.com>
2024-07-27 18:34:37 -0600

    Update data.js

--- src/app/data.js
+++ src/app/data.js
@@ -1,22 +1,162 @@
-export const data=[
-    { id: 1, title: 'Laptop Acer Nitro 5', price: 1150, urlImage: 'https://m.media-amazon.com/images/I/71Wkd-uyvnL.__AC_SY300_SX300_QL70_FMwebp_.jpg', quantity: 1 },
-    { id: 2, title: 'Ratón Razer Deathadder', price: 20, urlImage: 'https://siman.vtexassets.com/arquivos/ids/4337679-800-800?v=638218579472470000&width=800&height=800&aspect=true', quantity: 1 },
-    { id: 3, title: 'Cable HDMI', price: 7, urlImage: 'https://m.media-amazon.com/images/I/61RXLDFWz1L._AC_UF894,1000_QL80_.jpg', quantity: 1 },
-    { id: 4, title: 'Auriculares JBL Quantium 100', price: 50, urlImage: 'https://dcdn.mitiendanube.com/stores/003/731/505/products/5557551-b96592dd9caeab63aa16956909620027-1024-1024.jpg', quantity: 1 },
-    { id: 5, title: 'Disco duro Seagate Barracuda 2 TB', price: 85, urlImage: 'https://www.zonadigitalsv.com/product/ST2000DM008/image', quantity: 1 },
-    { id: 6, title: 'Microfono HyperX QuadCast', price: 150, urlImage: 'https://row.hyperx.com/cdn/shop/products/hyperx_quadcast_01_main_900x.jpg?v=1662449692', quantity: 1 },
-    { id: 7, title: 'RAM CORSAIR VENGEANCE 32 GB 4800 MHz', price: 85, urlImage: 'https://www.appinformatica.com/icecat/tcnova/CMSX8GX4M1A2400C16_pic_1.jpg', quantity: 1 },
-    { id: 8, title: 'Tarjeta Gráfica NVIDIA RTX 4060', price: 380, urlImage: 'https://m.media-amazon.com/images/I/71togGpKdcL._AC_UF894,1000_QL80_.jpg', quantity: 1 },
-    { id: 9, title: 'Intel i7-14700K', price: 400, urlImage: 'https://m.media-amazon.com/images/I/617EhDDXYfL.__AC_SX300_SY300_QL70_FMwebp_.jpg', quantity: 1 },
-    { id: 10, title: '970 EVO Plus NVMe™ M.2 SSD 2TB', price: 200, urlImage: 'https://images.samsung.com/is/image/samsung/uk-970-evoplus-nvme-m2-ssd-mz-v7s2t0bw-frontblack-319090477?$1300_1038_PNG$', quantity: 1 },
-    { id: 11, title: 'Windows 11 Pro', price: 100, urlImage: 'https://m.media-amazon.com/images/I/61Yacoqa0aL._AC_UF350,350_QL50_.jpg', quantity: 1 },
-    { id: 12, title: 'Fuente de Alimentación EVGA Supernova 750W', price: 100, urlImage: 'https://www.evga.com/products/images/gallery/110-BQ-0750-V1_XL_1.jpg', quantity: 1 },
-    { id: 13, title: 'Thermal Grizzly - Kryonaut Extreme', price: 10, urlImage: 'https://m.media-amazon.com/images/I/61fVUslbQoS._SX522_.jpg', quantity: 1 },
-    { id: 14, title: 'Thermal Grizzly - Conductonaut Extreme', price: 20, urlImage: 'https://m.media-amazon.com/images/I/61H92gKQbbL._AC_SX466_.jpg', quantity: 1 },
-    { id: 15, title: 'RAM Kingston FURY Impact 32 GB (2 x 16 GB)', price: 70, urlImage: 'https://m.media-amazon.com/images/I/71IWsVAkaCL._AC_SX466_.jpg', quantity: 1 },
-    { id: 16, title: 'Thermal Grizzly - KryoSheet', price: 22, urlImage: 'https://m.media-amazon.com/images/I/81qkgkNfPQL._AC_SX466_.jpg', quantity: 1 },
-    { id: 17, title: 'Lector tarjeta SD/microSD', price: 6, urlImage: 'https://m.media-amazon.com/images/I/51iUKVYrbyL._AC_SX466_.jpg', quantity: 1 },
-    { id: 18, title: 'HUB USB 3.0 de 4 puertos', price: 11, urlImage: 'https://www.steren.com.sv/media/catalog/product/cache/532829604b379f478db69368d14615cd/image/2250427f4/hub-usb-3-0-de-4-puertos.jpg', quantity: 1 },
-    { id: 19, title: 'RAM Crucial RAM 32 GB (2 x 16 GB) DDR5 5600 MHz', price: 85, urlImage: 'https://m.media-amazon.com/images/I/71k-boqf32L._AC_SX466_PIbundle-2,TopRight,0,0_SH20_.jpg', quantity: 1 },
-    { id: 20, title: 'Cámara Web Logitech C270 HD', price: 50, urlImage: 'https://walmartsv.vtexassets.com/arquivos/ids/196196/Camara-Web-Logitech-Usb-Audio-3Mpx-C270-2-420.jpg?v=637703518224630000', quantity: 1 }
-    ];
+export const data = [
+    {
+        id: 1,
+        title: 'Laptop Acer Nitro 5',
+        price: 1150,
+        urlImage: 'https://m.media-amazon.com/images/I/71Wkd-uyvnL.__AC_SY300_SX300_QL70_FMwebp_.jpg',
+        quantity: 1,
+        description: 'Laptop Acer Nitro 5 con procesador Intel 12.ª generación i7-12650H, RAM DDR5 de 16 GB , SSD de 1 TB Gen 4 y GPU NVIDIA GeForce RTX 4060.'
+    },
+    {
+        id: 2,
+        title: 'Ratón Razer Deathadder',
+        price: 20,
+        urlImage: 'https://siman.vtexassets.com/arquivos/ids/4337679-800-800?v=638218579472470000&width=800&height=800&aspect=true',
+        quantity: 1,
+        description: 'Ratón gaming Razer Deathadder con sensor óptico de alta precisión y diseño ergonómico.'
+    },
+    {
+        id: 3,
+        title: 'Cable HDMI',
+        price: 7,
+        urlImage: 'https://m.media-amazon.com/images/I/61RXLDFWz1L._AC_UF894,1000_QL80_.jpg',
+        quantity: 1,
+        description: 'Cable HDMI de alta velocidad con soporte para 4K y Ethernet.'
+    },
+    {
+        id: 4,
+        title: 'Auriculares JBL Quantium 100',
+        price: 50,
+        urlImage: 'https://dcdn.mitiendanube.com/stores/003/731/505/products/5557551-b96592dd9caeab63aa16956909620027-1024-1024.jpg',
+        quantity: 1,
+        description: 'Auriculares JBL Quantium 100 con sonido envolvente y micrófono incorporado.'
+    },
+    {
+        id: 5,
+        title: 'Disco duro Seagate Barracuda 2 TB',
+        price: 85,
+        urlImage: 'https://www.zonadigitalsv.com/product/ST2000DM008/image',
+        quantity: 1,
+        description: 'Disco duro interno Seagate Barracuda de 2TB con velocidad de 7200 RPM, Formato SATA de 3.5".'
+    },
+    {
+        id: 6,
+        title: 'Microfono HyperX QuadCast',
+        price: 150,
+        urlImage: 'https://row.hyperx.com/cdn/shop/products/hyperx_quadcast_01_main_900x.jpg?v=1662449692',
+        quantity: 1,
+        description: 'Micrófono HyperX QuadCast con patrón de captura ajustable y filtro pop integrado.'
+    },
+    {
+        id: 7,
+        title: 'RAM CORSAIR VENGEANCE 32 GB 4800 MHz',
+        price: 85,
+        urlImage: 'https://www.appinformatica.com/icecat/tcnova/CMSX8GX4M1A2400C16_pic_1.jpg',
+        quantity: 1,
+        description: 'Memoria RAM Corsair Vengeance de 32GB DDR5 a 4800 MHz.'
+    },
+    {
+        id: 8,
+        title: 'Tarjeta Gráfica NVIDIA RTX 4060',
+        price: 380,
+        urlImage: 'https://m.media-amazon.com/images/I/71togGpKdcL._AC_UF894,1000_QL80_.jpg',
+        quantity: 1,
+        description: 'Tarjeta gráfica NVIDIA RTX 4060 con 8GB de memoria GDDR6 y soporte para Ray Tracing.'
+    },
+    {
+        id: 9,
+        title: 'Intel i7-14700K',
+        price: 400,
+        urlImage: 'https://m.media-amazon.com/images/I/617EhDDXYfL.__AC_SX300_SY300_QL70_FMwebp_.jpg',
+        quantity: 1,
+        description: 'Procesador Intel i7-14700K de 12 núcleos y 24 hilos, con frecuencia base de 3.8 GHz.'
+    },
+    {
+        id: 10,
+        title: '970 EVO Plus NVMe™ M.2 SSD 2TB',
+        price: 200,
+        urlImage: 'https://images.samsung.com/is/image/samsung/uk-970-evoplus-nvme-m2-ssd-mz-v7s2t0bw-frontblack-319090477?$1300_1038_PNG$',
+        quantity: 1,
+        description: 'SSD NVMe M.2 Samsung 970 EVO Plus de 2TB con velocidades de lectura de hasta 3500 MB/s.'
+    },
+    {
+        id: 11,
+        title: 'Windows 11 Pro',
+        price: 100,
+        urlImage: 'https://m.media-amazon.com/images/I/61Yacoqa0aL._AC_UF350,350_QL50_.jpg',
+        quantity: 1,
+        description: 'Sistema operativo Windows 11 Pro, ideal para empresas y usuarios avanzados.'
+    },
+    {
+        id: 12,
+        title: 'Fuente de Alimentación EVGA Supernova 750W',
+        price: 100,
+        urlImage: 'https://www.evga.com/products/images/gallery/110-BQ-0750-V1_XL_1.jpg',
+        quantity: 1,
+        description: 'Fuente de alimentación EVGA Supernova de 750W con certificación 80 Plus Bronze.'
+    },
+    {
+        id: 13,
+        title: 'Thermal Grizzly - Kryonaut Extreme',
+        price: 10,
+        urlImage: 'https://m.media-amazon.com/images/I/61fVUslbQoS._SX522_.jpg',
+        quantity: 1,
+        description: 'Pasta térmica Thermal Grizzly Kryonaut Extreme para overclocking y sistemas de alto rendimiento.'
+    },
+    {
+        id: 14,
+        title: 'Thermal Grizzly - Conductonaut Extreme',
+        price: 20,
+        urlImage: 'https://m.media-amazon.com/images/I/61H92gKQbbL._AC_SX466_.jpg',
+        quantity: 1,
+        description: 'Pasta térmica conductora de metal líquido Thermal Grizzly Conductonaut Extreme.'
+    },
+    {
+        id: 15,
+        title: 'RAM Kingston FURY Impact 32 GB (2 x 16 GB)',
+        price: 70,
+        urlImage: 'https://m.media-amazon.com/images/I/71IWsVAkaCL._AC_SX466_.jpg',
+        quantity: 1,
+        description: 'Memoria RAM Kingston FURY Impact de 32GB (2 x 16GB) DDR5 a 4800 MHz.'
+    },
+    {
+        id: 16,
+        title: 'Thermal Grizzly - KryoSheet',
+        price: 22,
+        urlImage: 'https://m.media-amazon.com/images/I/81qkgkNfPQL._AC_SX466_.jpg',
+        quantity: 1,
+        description: 'Hoja térmica Thermal Grizzly KryoSheet para una excelente conductividad térmica.'
+    },
+    {
+        id: 17,
+        title: 'Lector tarjeta SD/microSD',
+        price: 6,
+        urlImage: 'https://m.media-amazon.com/images/I/51iUKVYrbyL._AC_SX466_.jpg',
+        quantity: 1,
+        description: 'Lector de tarjetas SD/microSD con conectividad USB 3.0 para transferencias rápidas.'
+    },
+    {
+        id: 18,
+        title: 'HUB USB 3.0 de 4 puertos',
+        price: 11,
+        urlImage: 'https://www.steren.com.sv/media/catalog/product/cache/532829604b379f478db69368d14615cd/image/2250427f4/hub-usb-3-0-de-4-puertos.jpg',
+        quantity: 1,
+        description: 'HUB USB 3.0 de 4 puertos para ampliar la conectividad de tu PC o laptop.'
+    },
+    {
+        id: 19,
+        title: 'RAM Crucial RAM 32 GB (2 x 16 GB) DDR5 5600 MHz',
+        price: 85,
+        urlImage: 'https://m.media-amazon.com/images/I/71k-boqf32L._AC_SX466_PIbundle-2,TopRight,0,0_SH20_.jpg',
+        quantity: 1,
+        description: 'Memoria RAM Crucial de 32GB (2 x 16GB) DDR5 a 5600 MHz para alto rendimiento.'
+    },
+    {
+        id: 20,
+        title: 'Cámara Web Logitech C270 HD',
+        price: 50,
+        urlImage: 'https://walmartsv.vtexassets.com/arquivos/ids/196196/Camara-Web-Logitech-Usb-Audio-3Mpx-C270-2-420.jpg?v=637703518224630000',
+        quantity: 1,
+        description: 'Cámara web Logitech C270 HD con micrófono integrado para videollamadas claras.'
+    }
+];
